972e166c5fa48034ff2aa8453b05b6aaaf883692
Tuding071 <davidpenaranda071@gmail.com>
2026-04-24 00:22:59 +0800

    Update Browser.kt

--- Browser.kt
+++ Browser.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RectangleShape          // ← REQUIRED
+import androidx.compose.ui.graphics.RectangleShape          // ← REQUIRED
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
